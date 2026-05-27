8c18dbd5f095f058355b7905036e65387a43d8a5
hjh <xiaodu4346@foxmail.com>
2026-05-27 21:07:01 +0800

    Add friend-specific chat histories

--- src/ChatWindow.cpp
+++ src/ChatWindow.cpp
@@ -3,6 +3,7 @@
 #include <QHBoxLayout>
 #include <QLabel>
 #include <QLineEdit>
+#include <QListWidget>
 #include <QPlainTextEdit>
 #include <QPushButton>
 #include <QVBoxLayout>
@@ -16,10 +17,16 @@ ChatWindow::ChatWindow(const QString &username, QWidget *parent)
 
     QLabel *titleLabel = new QLabel("ChatTest", this);
     QLabel *userLabel = new QLabel("Logged in as: " + username, this);
+    chatTargetLabel = new QLabel(this);
+
+    friendList = new QListWidget(this);
+    friendList->addItem("friendA");
+    friendList->addItem("friendB");
+    friendList->addItem("friendC");
+    friendList->addItem("friendD");
 
     messageView = new QPlainTextEdit(this);
     messageView->setReadOnly(true);
-    messageView->appendPlainText("System: Welcome to ChatTest.");
 
     messageEdit = new QLineEdit(this);
     messageEdit->setPlaceholderText("Type a message");
@@ -30,24 +37,56 @@ ChatWindow::ChatWindow(const QString &username, QWidget *parent)
     inputLayout->addWidget(messageEdit);
     inputLayout->addWidget(sendButton);
 
-    QVBoxLayout *mainLayout = new QVBoxLayout(this);
-    mainLayout->addWidget(titleLabel);
-    mainLayout->addWidget(userLabel);
-    mainLayout->addWidget(messageView);
-    mainLayout->addLayout(inputLayout);
+    QVBoxLayout *chatLayout = new QVBoxLayout;
+    chatLayout->addWidget(titleLabel);
+    chatLayout->addWidget(userLabel);
+    chatLayout->addWidget(chatTargetLabel);
+    chatLayout->addWidget(messageView);
+    chatLayout->addLayout(inputLayout);
+
+    QHBoxLayout *mainLayout = new QHBoxLayout(this);
+    mainLayout->addWidget(friendList);
+    mainLayout->addLayout(chatLayout);
 
     connect(sendButton, &QPushButton::clicked, this, &ChatWindow::handleSendMessage);
     connect(messageEdit, &QLineEdit::returnPressed, this, &ChatWindow::handleSendMessage);
+    connect(friendList, &QListWidget::currentTextChanged, this, &ChatWindow::handleFriendChanged);
+    friendList->setCurrentRow(0);
 }
 
 void ChatWindow::handleSendMessage()
 {
+    if (friendList->currentItem() == nullptr) {
+        return;
+    }
+
     const QString message = messageEdit->text().trimmed();
 
     if (message.isEmpty()) {
         return;
     }
 
-    messageView->appendPlainText(username + ": " + message);
+    appendMessage(username, message);
+    appendAutoReply(message);
     messageEdit->clear();
+}
+
+void ChatWindow::handleFriendChanged(const QString &friendName)
+{
+    chatTargetLabel->setText("chatting with: " + friendName);
+    messageView->setPlainText(chatHistory[friendName]);
+}
+
+void ChatWindow::appendMessage(const QString &sender, const QString &message)
+{
+    const QString friendName = friendList->currentItem()->text();
+    const QString chatLine = sender + ": " + message;
+    chatHistory[friendName] += chatLine + "\n";
+    messageView->appendPlainText(chatLine);
+}
+
+void ChatWindow::appendAutoReply(const QString &message)
+{
+    const QString friendName = friendList->currentItem()->text();
+    appendMessage(friendName, "I received: " + message);
 }
--- src/ChatWindow.h
+++ src/ChatWindow.h
@@ -1,9 +1,12 @@
 #pragma once
 
+#include <QMap>
 #include <QString>
 #include <QWidget>
 
 class QLineEdit;
+class QListWidget;
+class QLabel;
 class QPlainTextEdit;
 class QPushButton;
 
@@ -16,7 +19,13 @@ private:
     QString username;
     QPlainTextEdit *messageView;
     QLineEdit *messageEdit;
+    QListWidget *friendList;
+    QLabel *chatTargetLabel;
+    QMap<QString, QString> chatHistory;
     QPushButton *sendButton;
 
     void handleSendMessage();
+    void handleFriendChanged(const QString &friendName);
+    void appendMessage(const QString &sender, const QString &message);
+    void appendAutoReply(const QString &message);
 };
